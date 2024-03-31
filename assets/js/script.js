918261901195699fc6df2e5001bc414e5fc0f08f
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-03-31 13:35:04 +0000

    Add showAlert to show correctg answers or incorrect answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,46 @@ const questionBox = document.querySelector('.question');
 const optionsBox = document.querySelector('.options');
 const nextBtn = document.querySelector('.nextBtn');
 const scoreCard = document.querySelector('.scoreCard')
+const alert = document.querySelector('.alert');
 
 //Array of objecs which stores que and answer//
 const quiz = [
     {
-        question: "Q.Which planet has 145 moons?",
+        question: "Q1.Which planet has 145 moons?",
         options:["Saturn","Mars","Mercury","Jupiter"],
         answer: "Saturn"
     },
     {
-        question: "Q.In which country was the game of chess invented?",
+        question: "Q2.In which country was the game of chess invented?",
         options:["India","China","Greece","Japan"],
         answer: "India"
 
     },
     {
         
-        question: "Q.What was the first fruit to be eaten on the Moon?",
+        question: "Q3.What was the first fruit to be eaten on the Moon?",
         options:["Grapes","A peach","A starfruit","Apple"],
         answer:"A peach"
     },
     {
-        question: "Q.Which continent is home to the snow leopard?",
+        question: "Q4.Which continent is home to the snow leopard?",
         options:["North America","Asia","South America","Africa"],
         answer:"Asia"
 
     },
     {
-        question: "Q.What word appears on cold taps in Italy?",
+        question: "Q5.What word appears on cold taps in Italy?",
         options:["Freddo","Froggie","Bruno","Carlo"],
         answer:"Freddo"
 
     },
     {
-        question: "Q.Which country did Transylvania belong to from 11th century until 1918?",
+        question: "Q6.Which country did Transylvania belong to from 11th century until 1918?",
         options:["Germany","Romania","Britain","Hungary"],
         answer:"Hungary"
     },
     {
-        question: "Q.WHICH of these is NOT a recognised name for a young beaver?",
+        question: "Q7.WHICH of these is NOT a recognised name for a young beaver?",
         options:["Mouse","Kit","Pup","Kitten"],
         answer:"Mouse"
     }
@@ -57,7 +58,7 @@ const showQuestion = () => {
     questionBox.textContent = questionElement.question;
 
     optionsBox.textContent = "";
-    for(let i=0;i<questionElement.options.length;i++){
+    for(let i=0; i < questionElement.options.length; i++){
         const currentOption = questionElement.options[i];
         const optionDiv = document.createElement('div');
         optionDiv.textContent = currentOption;
@@ -66,9 +67,9 @@ const showQuestion = () => {
 
         optionDiv.addEventListener('click', ()=>{
             if(optionDiv.classList.contains('selected')){
-                optionDiv.classList.remove('selected');
+               optionDiv.classList.remove('selected');
             }else{
-                optionDiv.classList.remove('selected');
+                optionDiv.classList.add('selected');
             }
         });
     }
@@ -77,11 +78,12 @@ const showQuestion = () => {
     const checkAnswer = () => {
         const selectedOption = document.querySelector('.option.selected');
         if(selectedOption.textContent === quiz[currentQuestionIndex].answer){
-           alert("Correct Answer");
+           //alert("Correct Answer");
+           showAlert("Correct Answer");
            score++;
         }else{
-            alert("Wrong Answer");  
-        }  
+            //alert("Incorrect Answer");  
+        }  showAlert("Incorrect Answer");
         currentQuestionIndex++;
         if(currentQuestionIndex < quiz.length){
              showQuestion();
@@ -96,13 +98,19 @@ const showQuestion = () => {
         questionBox.textContent = "";
         optionsBox.textContent ="";
         scoreCard.textContent = `You scored ${score} out of ${quiz.length}`;
+        showAlert("You have completed your Quiz");
         nextBtn.textContent = "Play Again";
-        nextBtn.addEventListener('click', () =>{
-            currentQuestionIndex = 0;
-            showQuestion();
-            nextBtn.textContent = "Next";
-            scoreCard.textContent = ""; 
-        });
+       // nextBtn.addEventListener('click', () =>{
+          //  currentQuestionIndex = 0;
+           // showQuestion();
+           // nextBtn.textContent = "Next";
+           // scoreCard.textContent = ""; 
+        //});
+        }
+    //Function to show Alert
+        const showAlert = () =>{
+            alert.style.show = "block";
+            alert.textContent = message;
 
         }
 
@@ -110,7 +118,8 @@ const showQuestion = () => {
     nextBtn.addEventListener('click',()=>{
         const selectedOption = document.querySelector('.option.selected');
         if(!selectedOption && nextBtn.textContent === "Next"){
-            alert("Select your answer");
+            //alert("Select your answer");
+            showAlert("Select Your Answer");
             return;
         }
         if(quizCompleted){
