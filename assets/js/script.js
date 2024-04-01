8e1142b254720d03598c696bbb755553aad30d17
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-04-01 11:15:17 +0000

    Add quiz fevicon image to html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,12 +47,13 @@ const quiz = [
         question: "Q7.WHICH of these is NOT a recognised name for a young beaver?",
         options:["Mouse","Kit","Pup","Kitten"],
         answer:"Mouse"
-    }
+    },
     {
         question: "Q8.In Ancient Rome, how many days of the week were there?",
-        options:["Five","Eight","Six","Seven"],
+        options:["Five", "Eight", "Six", "Seven"],
         answer: "Eight"
     },
+
     {
         question: "Q9.What is the primary gas that makes up the Earth’s atmosphere?",
         options:["Oxygen","Nitrogen","Carbon dioxide","Hydrogen"],
@@ -67,8 +68,8 @@ const quiz = [
  //Variables
 let currentQuestionIndex = 0;
 let score = 0;
-let quizCompleted =false;
-let timeLeft = 10;
+let quizCompleted = false;
+let timeLeft = 30;
 let timerId = null;
 
 //Arrow functions
@@ -76,7 +77,7 @@ const showQuestion = () => {
     //console.log("Question");
     const questionElement = quiz[currentQuestionIndex];
     questionBox.textContent = questionElement.question;
-
+ 
     optionsBox.textContent = "";
     for(let i=0; i < questionElement.options.length; i++){
         const currentOption = questionElement.options[i];
@@ -108,20 +109,21 @@ const showQuestion = () => {
         }else{
             displayAlert(`Incorrect Answer!! ${quiz[currentQuestionIndex].answer} is correct Answer`);  
         }  
-        timeLeft = 10;
+        timeLeft = 30;
         currentQuestionIndex++;
         if(currentQuestionIndex < quiz.length){
              showQuestion();
          }else{
-            showScore();
             stopTimer();
+            showScore(); 
+            quizCompleted = true;
          }
     }
 
     //Function to show score
-    const showScore = () =>{
+    const showScore = () => {
         questionBox.textContent = "";
-        optionsBox.textContent ="";
+        optionsBox.textContent = "";
         scoreCard.textContent = `You scored ${score} out of ${quiz.length}`;
         displayAlert("You have completed your Quiz");
         nextBtn.textContent = "Play Again";
@@ -130,10 +132,10 @@ const showQuestion = () => {
         } 
 
     //Function to show Alert
-        const displayAlert = (message) =>{
+        const displayAlert = (message) => {
             alert.style.display = "block";
             alert.textContent = message;
-            setTimeUp(() =>{
+            setTimeout(() => { 
                 alert.style.display = "none";
             }, 1500);
 
@@ -143,14 +145,15 @@ const showQuestion = () => {
         const startTimer = () =>{
             clearInterval(timerId);
             timer.textContent = timeLeft;
+
             
             const countDown = () =>{
                 timeLeft--;
                 timer.textContent = timeLeft;
                 if(timeLeft === 0){
-                    const confirmUser = confirm("Time out !! >Would you like another go?");
+                    const confirmUser = confirm("Time out !! Would you like another go?");
                     if(confirmUser){
-                        timeLeft=10;
+                        timeLeft=30;
                         startGame();
                     }else{
                         startBtn.style.display ="block";
@@ -161,15 +164,17 @@ const showQuestion = () => {
             }
             timerId = setInterval(countDown,1000);
         }
-    //Function to Stop timer after 10 countdowns
+    //Function to Stop timer after 30 countdowns
     const stopTimer = () =>{
         clearInterval(timerId);
     }
+    
         const startGame = () => {
-            timeLeft = 10;
+            timeLeft = 30;
             timer.style.display = "flex";
             showQuestion();
         }
+
     //Add Event Listner to Start Btn
     startBtn.addEventListener('click',() =>{
         startBtn.style.display ="none";
@@ -178,7 +183,7 @@ const showQuestion = () => {
     });
 
     //showQuestion();
-    nextBtn.addEventListener('click',()=>{
+    nextBtn.addEventListener('click',() =>{
         const selectedOption = document.querySelector('.option.selected');
         if(!selectedOption && nextBtn.textContent === "Next"){
             //alert("Select your answer");
