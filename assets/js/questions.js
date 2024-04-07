76d6ca7e288c19d471994fcabdc819e1dc6f077c
Vijaylaxmi Biradar <vijaylaxmipb@gmail.com>
2024-04-07 14:40:29 +0000

    Add questions.js for quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,61 @@
+//Array of objecs which stores que and answer//
+
+const quiz = [
+    {
+        question: "Q.Which planet has 145 moons?",
+        options:["Saturn","Mars","Mercury","Jupiter"],
+        answer: "Saturn"
+    },
+    {
+        question: "Q.In which country was the game of chess invented?",
+        options:["India","China","Greece","Japan"],
+        answer: "India"
+
+    },
+    {
+        
+        question: "Q.What was the first fruit to be eaten on the Moon?",
+        options:["Grapes","A peach","A starfruit","Apple"],
+        answer:"A peach"
+    },
+    {
+        question: "Q.Which continent is home to the snow leopard?",
+        options:["North America","Asia","South America","Africa"],
+        answer:"Asia"
+
+    },
+    {
+        question: "Q.What word appears on cold taps in Italy?",
+        options:["Freddo","Froggie","Bruno","Carlo"],
+        answer:"Freddo"
+
+    },
+    {
+        question: "Q.Which country did Transylvania belong to from 11th century until 1918?",
+        options:["Germany","Romania","Britain","Hungary"],
+        answer:"Hungary"
+    },
+    {
+        question: "Q.WHICH of these is NOT a recognised name for a young beaver?",
+        options:["Mouse","Kit","Pup","Kitten"],
+        answer:"Mouse"
+    },
+    {
+        question: "Q.In Ancient Rome, how many days of the week were there?",
+        options:["Five", "Eight", "Six", "Seven"],
+        answer: "Eight"
+    },
+
+    {
+        question: "Q.What is the primary gas that makes up the Earth’s atmosphere?",
+        options:["Oxygen","Nitrogen","Carbon dioxide","Hydrogen"],
+        answer: "Nitrogen"
+    },
+    {
+        question: "Q.When was the world’s first ATM introduced, in Enfield, UK?",
+        options:["1967","1977","1987","1997"],
+        answer: "1967"
+    },
+];
+
+export default quiz;
